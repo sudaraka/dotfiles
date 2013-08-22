82f47289e16b385c9238f133e247a808a9ed278b
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2013-08-23 01:06:00 +0530

    Initial commit
    Creating repository with breif description, license information and skeleton
    setup script.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# setup.sh: install configuration files into current user's environment.
+# Author: Sudaraka Wijesinghe
+#
+
+echo 'Dotfiles Setup.'
+echo '==============='
+echo 'Copyright 2013 Sudaraka Wijesinghe. <sudaraka.org/contact>'
+echo 'Creative Commons Attribution 3.0 Unported License.'
+echo
+
+
